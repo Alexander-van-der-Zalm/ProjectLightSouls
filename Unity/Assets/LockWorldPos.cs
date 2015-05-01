cd253a49e950bddec82db69531260ce98958bf01
Alexander van der Zalm <alexandervdzalm@gmail.com>
2015-05-01 15:28:56 +0200

    Animator is soooooo happppyyy peppie

---
+++ Unity/Assets/LockWorldPos.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LockWorldPos : MonoBehaviour 
+{
+	public bool LockWorldPosition = false;
+	private Vector3 pos;
+	private Transform tr;
+
+	// Use this for initialization
+	void Start () 
+	{
+		tr = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!LockWorldPosition)
+			pos = tr.position;
+		else
+			tr.position = pos;
+	}
+}
